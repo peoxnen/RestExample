3742902b91f2179fce25894f208d57a06787aca3
peoxnen <sienski.witold@gmail.com>
2018-11-21 21:09:02 +0100

    add glide, ad custom apdater for avatar url

---
+++ app/src/main/java/iview/wsienski/restexample/CustomBindings.kt
@@ -0,0 +1,21 @@
+package iview.wsienski.restexample
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+
+
+/**
+ * Created by Witold Sienski on 21.11.2018.
+ */
+@BindingAdapter("bind:imageUrl")
+fun setImageUrl(view: ImageView, url: String) {
+    val options = RequestOptions()
+        .placeholder(android.R.drawable.btn_star)
+
+    Glide.with(view.context)
+        .setDefaultRequestOptions(options)
+        .load(url)
+        .into(view)
+}
