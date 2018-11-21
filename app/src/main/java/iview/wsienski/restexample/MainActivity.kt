4adf5186b61619fde2b74f8771dded13a7065ca9
peoxnen <sienski.witold@gmail.com>
2018-11-21 01:14:54 +0100

    add findFragmentByTag

--- app/src/main/java/iview/wsienski/restexample/MainActivity.kt
+++ app/src/main/java/iview/wsienski/restexample/MainActivity.kt
@@ -10,10 +10,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
+            val newInstance = supportFragmentManager.findFragmentByTag(TAG) ?: MainFragment.newInstance()
             supportFragmentManager.beginTransaction()
-                .replace(R .id.container, MainFragment.newInstance())
+                .replace(R.id.container, newInstance, TAG)
                 .commitNow()
         }
+    }
+
+    companion object {
+        const val TAG = "MAIN_FRAGMENT_TAG"
     }
 
 }
